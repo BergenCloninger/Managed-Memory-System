58dd8e970cdcdac0f8ca0d99d43907fb0dd8ca44
Intaiachi <mjones21031@gmail.com>
2022-09-29 18:43:57 -0600

    final

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main() {
 	global::buffer = new unsigned char[65536];
-	Pointer<int> testPointer = reinterpret_cast<int*>(mallocbutswag(4));
+	int* testPointer = reinterpret_cast<int*>(mallocbutswag(4));
 	*testPointer = 3;
 	std::cout << *testPointer;
 	return 0;
